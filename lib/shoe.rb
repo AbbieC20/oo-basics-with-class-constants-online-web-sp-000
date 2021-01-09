d390c83340c84faaf37b23bbff601913cb1e8d21
remote-torrent-6747 <abbyc@sky.com>
2021-01-09 15:57:02 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,11 +1,11 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
-  attr_reader :brand
+  attr_reader :brands
 
   BRANDS = [ ]
 
-  def initialize(brand)
-    @brand = brand
+  def initialize(brands)
+    @brands = brands
   end
 
   def cobble
@@ -13,9 +13,9 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
 
-  def brand= (brand)
-    @brand = brand
-    BRANDS << brand
+  def brands= (brands)
+    @brands = brands
+    BRANDS << brands
   end
 
 end
